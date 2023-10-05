36279d8e31c5676e49898f3b21dfc3beb124e9cf
André ÃLacerda <dedelacerda95@gmail.com>
2023-10-05 14:27:34 -0300

    criei tabela de professionals e volunteers, mas a de user nao pôde ser gerada

---
+++ db/migrate/20231004192536_create_professionals.rb
@@ -0,0 +1,9 @@
+class CreateProfessionals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :professionals do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
