e22eb8506046fc1e70affc54b084b1dde89f148f
Kaique Costa <kaiquenagato94@outlook.com>
2023-10-10 16:01:14 -0300

    checkbox roles funcionam agora

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user.update
+    @user.update(user_params)
     redirect_to users_path
   end
 
@@ -17,5 +17,9 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :age, :phone, :city, :state, :country, :address, :instagram, :profession, :professional_registration, :specialty, role_ids: [])
   end
 end
