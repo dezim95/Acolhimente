6b0b64f449035919bd7a4827a98886688d849b81
Kaique Costa <kaiquenagato94@outlook.com>
2023-10-16 14:20:01 -0300

    model de comment

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -7,23 +7,11 @@ class ForumThreadsController < ApplicationController
 
   def show
     @forum_thread = ForumThread.find(params[:id])
-    @comment = Comment.new
   end
 
   def new
     @forum_thread = ForumThread.new
     @forum_thread.forum_posts.build
-  end
-
-  def create_comment
-    @forum_thread = ForumThread.find(params[:id])
-    @comment = @forum_thread.comments.build(params.require(:comment).permit(:body))
-    @comment.user_id = current_user.id
-    if @comment.save
-      redirect_to @forum_thread, notice: 'Comment was successfully created.'
-    else
-      render :show
-    end
   end
 
   def create
@@ -38,10 +26,6 @@ class ForumThreadsController < ApplicationController
   end
 
   private
-
-  def comment_params
-    params.require(:comment).permit(:body)
-  end
 
   def set_forum_thread
     @forum_thread = ForumThread.find(params[:id])
