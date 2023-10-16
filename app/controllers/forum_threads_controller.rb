a12f2c34644097676bbbd233da53ecc60033423c
Kaique Costa <kaiquenagato94@outlook.com>
2023-10-16 14:27:37 -0300

    voltando somente ao model de comment

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -17,7 +17,6 @@ class ForumThreadsController < ApplicationController
   def create
     @forum_thread = current_user.forum_threads.build(forum_thread_params)
     @forum_thread.forum_posts.first.user_id = current_user.id
-
     if @forum_thread.save
       redirect_to @forum_thread, notice: 'Thread was successfully created.'
     else
