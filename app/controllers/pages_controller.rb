732f059e52434231809d4a0e40da15a625c79418
André ÃLacerda <dedelacerda95@gmail.com>
2023-10-31 17:14:00 -0300

    implementamos botao sos beta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :emergency_form]
 
   def home
+  end
+
+  def emergency_form
   end
 end
