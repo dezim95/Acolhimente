c9527163f061c752a9cdeb8b249025aa282bf714
Andre Gorgen <andregorgen@gmail.com>
2023-11-07 16:25:49 -0300

    mudancas de rotas, loading page e formulario

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :emergency_form]
+  skip_before_action :authenticate_user!, only: [:home, :emergency_form, :loading_page]
 
   def home
   end
 
   def emergency_form
+  end
+
+  def loading_page
   end
 end
